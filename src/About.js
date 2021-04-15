bbff6022bedfc481c392fa3c236a7915fece72c0
PRANAV MANI TRIPATHI <pranavtripathikishan26@gmail.com>
2021-04-15 23:28:23 +0530

    added files

---
+++ src/About.js
@@ -0,0 +1,19 @@
+const About = () => {
+    return (  
+        <div className="about" id="about">
+             <h1>WHY NATURE NEEDS TO BE PROTECTED</h1>
+             <p className="about-para">The most obvious reason for conservation is to protect wildlife and promote biodiversity. Protecting wildlife and preserving it for future generations also means that the animals we love don’t become a distant memory. And we can maintain a healthy and functional ecosystem. It’s no secret that the future of our planet desperately needs to be safeguarded, with climate change already wreaking havoc on our natural environment. In order to preserve the earth for future generations, we not only need to reduce the amount of harm that human activities have on the environment but support the natural world as much as we can.One pretty big reason for conservation work that is talked about a little less often is the impact that it has on human health. Both in terms of preventing the emergence of new diseases, and the production of medicines that we rely upon.</p>
+             
+             <h1>ABOUT US</h1>
+             <p className="about-para" > WWF-Laos is the local office of the WWF International Network, the world’s largest and most experienced independent conservation organisation. It has more than 5 million supporters and offices active in over 100 countries.
+             <br></br> WWF’s mission is to stop the degradation of the planet’s natural environment and to build a future in which humans live in harmony with nature. To do this, WWF works with a broad spectrum of partners including governments, industry, and local communities to find solutions to the challenges that face our natural world.</p>
+
+
+             </div>
+
+        
+
+    );
+}
+ 
+export default About;
---
+++ src/Navbar.js
@@ -0,0 +1,19 @@
+
+import logo from './wwflogo1.jpg';
+const Navbar = () => {
+    return (
+        <div className="nature-image">
+            <nav className = "navbar">
+                <img src={ logo }  alt="" className="logo-image" href="/"></img>
+                <div className="links">
+                    <a href="/" className="class-link">Home</a>
+                    <a href="#about" className="class-link">About Us</a>
+                    <a href="#contact" className="class-link">Contact Us</a>
+                </div>
+            </nav>
+            <a href="https://rzp.io/l/tR3rsFi" className="donate-button"  >DONATE</a>
+        </div> 
+    );
+}
+ 
+export default Navbar;
